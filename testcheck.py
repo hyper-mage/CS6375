1b73d1a70201ded47021eba29d424cfc4a4e3582
hyper-mage <mlynn808138@gmail.com>
2019-11-25 16:31:06 -0600

    used list comprehension for x_train and y_train

--- testcheck.py
+++ testcheck.py
@@ -27,22 +27,16 @@ train_data_scaled = scaler.fit_transform(train_data)
 
 #%% Slap some timesteps in they
 
-X_train = []
-y_train = []
-for i in range(60, len(data)):
-    X_train.append(train_data_scaled[i-60:i, 0])
-    y_train.append(train_data_scaled[i, 0])
-X_train, y_train = np.array(X_train), np.array(y_train)
-
 # Reshaping
 X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
 print("X_train:\n", X_train)
-print("y_train:\n", y_train)
 
 # List compre-way
-this = [train_data_scaled[i-60:i, 0] for i in range(60, len(data))]
-that = [train_data_scaled[i, 0] for i in range(60, len(data))]
-print("this\n", this, "\nthat\n", that)
-print("X_train == this", X_train == this)
-print("y_train == that", y_train == that)
+this = np.array([train_data_scaled[i-60:i, 0] for i in range(60, len(data))])
+that = np.array([train_data_scaled[i, 0] for i in range(60, len(data))])
+# Reshape dat b
+this = np.reshape(this, (this.shape[0], this.shape[1], 1))
+# print("X_train == this", (X_train == this).all())
+# print("y_train == that", (y_train == that).all()) # this checks out so we can just use the list comp
+
 
