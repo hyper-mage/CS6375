1e82d4c1169cefb68e7b3fd34e76ca1f99eebb37
hyper-mage <mlynn808138@gmail.com>
2019-11-25 16:48:42 -0600

    Added the lstm layers, and runnable

--- testcheck.py
+++ testcheck.py
@@ -1,5 +1,9 @@
 #%% Importante
 
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+from keras.layers import Dropout
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
@@ -27,10 +31,6 @@ train_data_scaled = scaler.fit_transform(train_data)
 
 #%% Slap some timesteps in they
 
-# Reshaping
-X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
-print("X_train:\n", X_train)
-
 # List compre-way
 this = np.array([train_data_scaled[i-60:i, 0] for i in range(60, len(data))])
 that = np.array([train_data_scaled[i, 0] for i in range(60, len(data))])
@@ -39,4 +39,32 @@ this = np.reshape(this, (this.shape[0], this.shape[1], 1))
 # print("X_train == this", (X_train == this).all())
 # print("y_train == that", (y_train == that).all()) # this checks out so we can just use the list comp
 
+#%% Construct the RNN architecture
+
+rnn = Sequential()
+
+# Adding the first LSTM layer and some Dropout regularisation
+rnn.add(LSTM(units = 50, return_sequences = True, input_shape = (this.shape[1], 1)))
+rnn.add(Dropout(0.2))
+
+# Adding a second LSTM layer and some Dropout regularisation
+rnn.add(LSTM(units = 50, return_sequences = True))
+rnn.add(Dropout(0.2))
+
+# Adding a third LSTM layer and some Dropout regularisation
+rnn.add(LSTM(units = 50, return_sequences = True))
+rnn.add(Dropout(0.2))
+
+# Adding a fourth LSTM layer and some Dropout regularisation
+rnn.add(LSTM(units = 50))
+rnn.add(Dropout(0.2))
+
+# Adding the output layer
+rnn.add(Dense(units = 1))
+
+# Compiling the RNN
+rnn.compile(optimizer = 'adam', loss = 'mean_squared_error')
+
+# Fitting the RNN to the Training set
+rnn.fit(this, that, epochs = 5, batch_size = 64)
 
