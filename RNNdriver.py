8075c58ba6f351502be6ce87ad02770fb282a7d7
hyper-mage <mlynn808138@gmail.com>
2019-11-28 15:41:20 -0600

    Changed driver file to poop out 5 different outputs for 1,2,3,7,and 30 day out models. Most of the new files here are output graphs and results

--- RNNdriver.py
+++ RNNdriver.py
@@ -1,31 +1,33 @@
 import RNNfunction as rnn
 
+layers = 5
+
 # 1 day out
 timesteps = 1
 train_X, train_y, test_X, test_y, df = rnn.prepare('DJI.csv',
                                                    header=True, testsize=0.5, preprocess=True, timesteps=timesteps)
-rnn.RNN(df, train_X, train_y, test_X, test_y, layers=2, timesteps=timesteps)
+rnn.RNN(df, train_X, train_y, test_X, test_y, layers=layers, timesteps=timesteps)
 
 # 2 days out
 timesteps = 2
 train_X, train_y, test_X, test_y, df = rnn.prepare('DJI.csv',
                                                    header=True, testsize=0.5, preprocess=True, timesteps=timesteps)
-rnn.RNN(df, train_X, train_y, test_X, test_y, layers=2, timesteps=timesteps)
+rnn.RNN(df, train_X, train_y, test_X, test_y, layers=layers, timesteps=timesteps)
 
 # 3 days out
 timesteps = 3
 train_X, train_y, test_X, test_y, df = rnn.prepare('DJI.csv',
                                                    header=True, testsize=0.5, preprocess=True, timesteps=timesteps)
-rnn.RNN(df, train_X, train_y, test_X, test_y, layers=2, timesteps=timesteps)
+rnn.RNN(df, train_X, train_y, test_X, test_y, layers=layers, timesteps=timesteps)
 
 # 7 days out
 timesteps = 7
 train_X, train_y, test_X, test_y, df = rnn.prepare('DJI.csv',
                                                    header=True, testsize=0.5, preprocess=True, timesteps=timesteps)
-rnn.RNN(df, train_X, train_y, test_X, test_y, layers=2, timesteps=timesteps)
+rnn.RNN(df, train_X, train_y, test_X, test_y, layers=layers, timesteps=timesteps)
 
 # 30 days out
 timesteps = 30
 train_X, train_y, test_X, test_y, df = rnn.prepare('DJI.csv',
                                                    header=True, testsize=0.5, preprocess=True, timesteps=timesteps)
-rnn.RNN(df, train_X, train_y, test_X, test_y, layers=2, timesteps=timesteps)
+rnn.RNN(df, train_X, train_y, test_X, test_y, layers=layers, timesteps=timesteps)
